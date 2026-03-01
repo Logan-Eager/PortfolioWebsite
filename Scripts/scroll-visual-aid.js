ab215b672d9d5676cdbdf09acc14eb9c6adc085b
Logan <loganallaneager@gmail.com>
2026-03-01 18:02:47 +1300

    clean code and few bug fixes, add start of AOS script

--- Scripts/scroll-visual-aid.js
+++ Scripts/scroll-visual-aid.js
@@ -1,25 +1,15 @@
 // SCROLL VISUAL AID FUNCTION, (downwards arrow icon behaviour)
-
-// references
 const footer = document.querySelector("footer");
-
-// local vars
 let iconVisible = true; // flag as to wether it *should* be visible
 let inactivityTimer = null;
 
-document.addEventListener("scroll", handleScroll, { passive: true });
 
-// initial state
-if (footer) {
-  footer.classList.add("fade-in");
-}
-
-// triggers every scroll event
+// binded to scroll event, hides icon based on timer
 function handleScroll() {
   iconVisible = false;
   handleIconAnimation();
 
-  // resets timer
+  // reset timer
   if (inactivityTimer) clearTimeout(inactivityTimer);
   inactivityTimer = setTimeout(() => {
     iconVisible = true;
@@ -27,7 +17,7 @@ function handleScroll() {
   }, 5000); // 60,000ms == 1 min
 }
 
-// depending on flag, switches animation in 'buffer'
+// checks iconVisible flag, then applies anim classes to footer accordingly
 function handleIconAnimation() {
   if (!footer) return;
 
@@ -39,3 +29,13 @@ function handleIconAnimation() {
     footer.classList.add("fade-out");
   }
 }
+
+
+// MAIN EXECUTION
+if (footer) {
+  footer.classList.add("fade-in");
+}
+
+// call handleScroll everytime theres a scroll event
+document.addEventListener("scroll", handleScroll, { passive: true });
+
